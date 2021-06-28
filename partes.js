ba9cc5bfca3d3be058c0303112182e8c980a71db
RichardGraebin <richardgraebinjs@gmail.com>
2021-06-28 11:58:01 -0300

    Banco de Dados

--- partes.js
+++ partes.js
@@ -1,71 +1,23 @@
-/*let etapas = [
-    {
-        titulo: 'VEREADOR',
-        numeros: 5,
-        candidatos: [
-            {
-                numero: '38111',
-                nome: 'Fulano de Tal',
-                partido: 'ABC',
-                fotos:[
-                    {url:'38111.jpg', legenda: 'Vereador'}
-                ]},{
-                numero: '77222',
-                nome: 'Beltrano da Silva',
-                partido: 'DEFG',
-                fotos:[
-                    {url:'77222.jpg', legenda: 'Vereador'}
-                ]
-            },
-        ]
+let partes = [{
+        cargo: 'GOVERNADOR', digitos: 5, candidatos: [{
+            nome: 'Celso H.', numero: '36823', partido: 'CQM', fotos: [{
+                url:'36823.jpg', alt: 'Governador'
+            }]
+        },{
+            nome: 'Ricardo G.', numero: '77733', partido: 'PMF', fotos: [{
+                url: '77733.jpg', alt: 'Governador'
+            }]
+        }] 
     },
     {
-        titulo: 'PREFEITO',
-        numeros: 2,
-        candidatos: [
-            {
-                numero: '99',
-                nome: 'Ciclano',
-                partido: 'ABC',
-                vice: 'Cic',
-                fotos:[
-                    {url:'99.jpg', legenda: 'Prefeito'},
-                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
-                ]
-            },
-            {
-                numero: '84',
-                nome: 'Zulano',
-                partido: 'QWERTY',
-                vice: 'Zul',
-                fotos:[
-                    {url:'84.jpg', legenda: 'Prefeito'},
-                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
-                ]
-            },
-        ]
-    }
-];
-*/
-
-let partes = [{
-    cargo: 'GOVERNADOR', 
-    digitos: 5, 
-    candidatos: [{
-       nome: 'Celso H.', 
-       numero: '36823', 
-       partido: 'CQM', 
-       fotos: [{
-           url:'36823.jpg', 
-           alt: 'Governador'
-       }],
-    },{
-        nome: 'Ricardo G.',
-        numero: '77733',
-        partido: 'PMF',
-        fotos: [{
-            url: '77733.jpg',
-            alt: 'Governador'
+        cargo: 'PRESIDENTE', digitos: 2, candidatos: [{
+            nome: 'NICOLAS S.', numero: '66', partido: 'MTL', vice: 'Cesar S.', fotos: [{
+                url: '66.jpg', legenda: 'Presidente'},{url: '66_2.jpg', legenda: 'Vice-Presidente', small: true
+            }]
+        },{
+            nome: 'Vitor H.', numero: '21', partido: 'VLN', vice: 'Adalto H.', fotos : [{
+                url: '21.jpg', legenda: 'Presidente'},{url: '21_2.jpg', legenda: 'Vice-Presidente', small: true
+            }]
         }]
-    }] 
-}]
+    }
+]
