10f9558f22eafd338ed5c1685fc4b39005441b06
RichardGraebin <richardgraebinjs@gmail.com>
2021-07-01 12:02:43 -0300

    Adding JS

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let infos = document.querySelector('.geral-infos')
 let instrucoes = document.querySelector('.info')
 let images = document.querySelector('.screen-right')
 let numero = document.querySelector('.code')
+
+function clicked(n) {
+    window.alert(`Dígito ${n} pressionado.`)
+}
+
+function branco() {}
+
+function corrige() {}
+
+function confirma() {}
