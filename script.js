978c2aa49631e425fc7897ea3dc230116a558703
RichardGraebin <richardgraebinjs@gmail.com>
2021-07-01 13:03:35 -0300

    JS Update

--- script.js
+++ script.js
@@ -5,6 +5,33 @@ let instrucoes = document.querySelector('.info')
 let images = document.querySelector('.screen-right')
 let numero = document.querySelector('.code')
 
+let parteAtual = 0
+
+
+function start() {
+    let parte = partes[parteAtual]
+    let qntDigitos = ''
+
+   /* for(let i in parte.digitos) {
+        qndDigitos += '<div class="code-number"></div>'
+    }*/
+
+    for(let i=0; i<parte.digitos;i++) {
+        if(i === 0) {
+            qntDigitos += "<div class='code-number blink'></div>"
+        } else {
+            qntDigitos += "<div class='code-number'></div>"
+        }
+    }
+
+    role.innerHTML = parte.cargo
+    textSVP.style.display = 'none'
+    instrucoes.style.display = 'none'
+    infos.style.display = 'none' 
+    images.style.display = 'none'
+    numero.innerHTML = qntDigitos
+}
+
 function clicked(n) {
     window.alert(`Dígito ${n} pressionado.`)
 }
@@ -14,3 +41,5 @@ function branco() {}
 function corrige() {}
 
 function confirma() {}
+
+start()
